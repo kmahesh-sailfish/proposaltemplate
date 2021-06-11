32a0693552ac6a1757a365cac31d20f0fc165337
DESKTOP-KRJT632\sanja <v-sanjaysa@microsoft.com>
2021-06-11 00:35:02 -0500

    ag

--- src/app/proposal/modeal/search-proposal/search-proposal.component.ts
+++ src/app/proposal/modeal/search-proposal/search-proposal.component.ts
@@ -228,4 +228,66 @@ export class SearchProposalComponent implements OnInit {
       size: "lg"
     });
   }
+//Ag grid code
+  
+
+
+var gridOptions = {
+  rowData: getRowData(),
+
+  columnDefs: [
+    {
+      headerName: 'Set Filter Column',
+      field: 'col1',
+      filter: 'agSetColumnFilter',
+      editable: true,
+      flex: 1,
+    },
+  ],
+  sideBar: ['filters'],
+  onFirstDataRendered: onFirstDataRendered,
+};
+
+function getRowData() {
+  return [
+    { col1: 'A' },
+    { col1: 'A' },
+    { col1: 'D' },
+    { col1: 'B' },
+    { col1: 'C' },
+    { col1: 'C' },
+  ];
+}
+
+
+function updateFirstRow() {
+  var firstRow = gridOptions.api.getDisplayedRowAtIndex(0);
+  console.document.add(firstRow);
+  if (firstRow) {
+    var firstRowData = firstRow.data;
+    firstRowData['col1'] += 'X';
+     console.document.add(firstRow);
+    gridOptions.api.applyTransaction({ update: [firstRowData] });
+  }
+}
+
+function addDRow() {
+  gridOptions.api.applyTransaction({ add: [{ col1: 'D' }] });
+}
+
+function reset() {
+  gridOptions.api.setRowData(getRowData());
+}
+
+function onFirstDataRendered(params) {
+  params.api.getToolPanelInstance('filters').expandFilters();
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid');
+  new agGrid.Grid(gridDiv, gridOptions);
+});
+
+//Ag grid code end
 }
