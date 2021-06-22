e8e05472180b55bad8649e1b57ec2a641324550e
suslavkrishna@gmail.com <suslavkrishna@gmail.com>
2021-06-22 22:12:42 +0530

    add the active proposal  fxied

--- src/app/proposal/active-proposal/active-proposal.component.ts
+++ src/app/proposal/active-proposal/active-proposal.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-active-proposal',
-  templateUrl: './active-proposal.component.html',
-  styleUrls: ['./active-proposal.component.css']
+  selector: "app-active-proposal",
+  templateUrl: "./active-proposal.component.html",
+  styleUrls: ["./active-proposal.component.css"]
 })
 export class ActiveProposalComponent implements OnInit {
-
-  constructor() { }
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+  constructor() {}
 
   ngOnInit(): void {
+    this.dtTrigger.next();
   }
-
 }
