4777a0ab1b9ee79b7953fd71261000a2c3394632
suslavkrishna@gmail.com <suslavkrishna@gmail.com>
2021-06-23 19:38:51 +0530

    add the code

--- src/app/proposal/active-proposal/active-proposal.component.ts
+++ src/app/proposal/active-proposal/active-proposal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Subject } from "rxjs";
 
 @Component({
@@ -6,12 +6,33 @@ import { Subject } from "rxjs";
   templateUrl: "./active-proposal.component.html",
   styleUrls: ["./active-proposal.component.css"]
 })
-export class ActiveProposalComponent implements OnInit {
-  dtOptions: DataTables.Settings = {};
-  dtTrigger: Subject<any> = new Subject<any>();
+export class ActiveProposalComponent implements OnInit, OnDestroy {
+  public dtOptions: DataTables.Settings = {};
+  public dtTrigger: Subject<any> = new Subject<any>();
+  public ActiveProposal: any[] = [];
+
   constructor() {}
 
   ngOnInit(): void {
+    this.loadActiveProposal();
+  }
+  loadActiveProposal() {
+    this.ActiveProposal = [
+      {
+        create: "22/06/21",
+        proposalId: "225551",
+        createdby: "Sanjay",
+        modifiedby: "Sanjay v2",
+        customerName: "Sintel",
+        status: "created",
+        shared: "yes",
+        delegation: "no"
+      }
+    ];
     this.dtTrigger.next();
+  }
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
   }
 }
