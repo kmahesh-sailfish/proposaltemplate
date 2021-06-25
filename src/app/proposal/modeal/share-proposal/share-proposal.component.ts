1bd3206f73bf1225eb036f2624662ff5e4156066
DESKTOP-KRJT632\sanja <v-sanjaysa@microsoft.com>
2021-06-25 10:22:49 -0500

    new changes

--- src/app/proposal/modeal/share-proposal/share-proposal.component.ts
+++ src/app/proposal/modeal/share-proposal/share-proposal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm, FormGroup, FormControl, Validators } from "@angular/forms";
+
 import { Router } from "@angular/router";
 import {
   
@@ -12,23 +13,38 @@ import {
 })
 export class ShareProposalComponent implements OnInit {
   myForm: FormGroup;
+  emailErrorMessage:any
   constructor(private router: Router,
    private activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
     this.loadForm();
   }
+  checkField(){
+    if(this.myForm.get('flexRadioDefault').value == 'microsoft' && 
+    this.myForm.get('userEmail').value =="" ){
+      this.emailErrorMessage="'Please provide user's alias without \"@microsoft.com\'";
+    }else{
+      this.emailErrorMessage="It looks like Microsoft User. Please change user type";
+    }
+  }
   handleChange(event) {
     console.log("thi", this.myForm.get("flexRadioDefault").value);
+    // if(this.myForm.get("flexRadioDefault").value){
+
+    // }else{
+
+    // }
   }
   onSubmit() {
-    this.router.navigate(["activeproposal/"]);
+   // this.router.navigate(["activeproposal/"]);
     this.activeModal.close();
   }
   loadForm() {
+    let patter = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
     this.myForm = new FormGroup({
       flexRadioDefault: new FormControl("microsoft", Validators.required),
-      userEmail: new FormControl("", Validators.required)
+      userEmail: new FormControl("", [Validators.required,Validators.pattern(patter)])
     });
   }
 }
