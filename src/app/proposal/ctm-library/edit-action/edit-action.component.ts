b7be9c2a34ae206788465d339add262abbcf0e9d
suslavkrishna@gmail.com <suslavkrishna@gmail.com>
2021-08-04 19:32:48 +0530

    remove the unwanted code

--- src/app/proposal/ctm-library/edit-action/edit-action.component.ts
+++ src/app/proposal/ctm-library/edit-action/edit-action.component.ts
@@ -1,34 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { ICellRendererParams } from 'ag-grid-community';
-import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { ProposalService } from '../../../proposal.service';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
-import { ConformationComponent } from '../conformation/conformation.component';
-
+import { Component, OnInit } from "@angular/core";
+import { ICellRendererParams } from "ag-grid-community";
+import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { ProposalService } from "../../../proposal.service";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
+import { ConformationComponent } from "../conformation/conformation.component";
 
 @Component({
-  selector: 'app-edit-action',
-  templateUrl: './edit-action.component.html',
-  styleUrls: ['./edit-action.component.css']
+  selector: "app-edit-action",
+  templateUrl: "./edit-action.component.html",
+  styleUrls: ["./edit-action.component.css"]
 })
 export class EditActionComponent implements OnInit {
- cellValue: any;
+  cellValue: any;
   params: any;
-  obj:any={};
+  obj: any = {};
   // private objactiveComponent: ActiveProposalComponent;
   ngOnInit(): void {
     console.log(this.obj, "edited");
-   // this.obj = this.params.data;
+    // this.obj = this.params.data;
   }
- 
+
   constructor(
     //  private objactiveComponents: ActiveProposalComponent,
     private modalService: NgbModal,
     private adminService: ProposalService,
     private toastr: ToastrService,
     private router: Router
-    
   ) {
     // this.objactiveComponent = objactiveComponents;
   }
@@ -36,68 +34,46 @@ export class EditActionComponent implements OnInit {
   editClicked(event) {
     event.stopPropagation();
     console.log(this.params.data, "edited");
-   // this.router.navigate(["proposaloverview/", this.params.data["id"]]);
+    // this.router.navigate(["proposaloverview/", this.params.data["id"]]);
   }
-  // deleteClicked(event) {
-  //   event.stopPropagation();
-  //   var rowData = this.params.data;
-  //   rowData['labelMessage'] = "Delete";
-  //   console.log(this.params.data, "deleted");
-  //   const modalRef = this.modalService.open(ConformationComponent, {
-  //     size: "sm"
-  //   });
-  //   modalRef.componentInstance.labelMessage = "Delete";
-  //    modalRef.componentInstance.rowObj = rowData;
-  // }
-  // archieveClicked(event) {
-  //   event.stopPropagation();
-  //   var rowData = this.params.data;
-  //   rowData['labelMessage'] = "Archive";
-  //   console.log(this.params.data, "archive");
-  //   const modalRef = this.modalService.open(ConformationComponent, {
-  //     size: "sm"
-  //   });
-  //   modalRef.componentInstance.rowObj = rowData;
-    
-  // }
   downloadCTMFile(event) {
     event.stopPropagation();
     var rowData = this.params.data;
-    this.adminService.downLoadCTM(rowData['id']).subscribe(data => {
+    this.adminService.downLoadCTM(rowData["id"]).subscribe(data => {
       console.log(data);
-    })
+    });
   }
   shareClicked(event) {
     event.stopPropagation();
     var rowData = this.params.data;
-    rowData['labelMessage'] = "Share";
+    rowData["labelMessage"] = "Share";
     console.log(this.params.data, "share");
     const modalRef = this.modalService.open(ConformationComponent, {
       size: "sm"
     });
     modalRef.componentInstance.rowObj = rowData;
   }
-   deleteClicked(event) {
+  deleteClicked(event) {
     event.stopPropagation();
     var rowData = this.params.data;
-    rowData['labelMessage'] = "Delete";
+    rowData["labelMessage"] = "Delete";
     console.log(this.params.data, "deleted");
     const modalRef = this.modalService.open(ConformationComponent, {
       size: "sm"
     });
     modalRef.componentInstance.labelMessage = "Delete";
-     modalRef.componentInstance.rowObj = rowData;
+    modalRef.componentInstance.rowObj = rowData;
   }
-  stopShareCTM(event){
+  stopShareCTM(event) {
     event.stopPropagation();
     var rowData = this.params.data;
-    rowData['labelMessage']="Stop Sharing CTM";
-    
+    rowData["labelMessage"] = "Stop Sharing CTM";
+
     const modalRef = this.modalService.open(ConformationComponent, {
       size: "sm"
     });
     modalRef.componentInstance.labelMessage = "Stop Sharing CTM";
-     modalRef.componentInstance.rowObj = rowData;
+    modalRef.componentInstance.rowObj = rowData;
   }
   agInit(params: ICellRendererParams): void {
     this.cellValue = params.value;
@@ -105,5 +81,4 @@ export class EditActionComponent implements OnInit {
     this.obj = params.data;
     //this.objactiveComponent.setHRDEditDiv(this.params.data);
   }
- 
 }
