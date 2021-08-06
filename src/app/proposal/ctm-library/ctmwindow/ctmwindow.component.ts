a821c599f59616fc7614a66f7e3db3801f90706b
suslavkrishna@gmail.com <suslavkrishna@gmail.com>
2021-08-06 20:40:23 +0530

    fixed the issues

--- src/app/proposal/ctm-library/ctmwindow/ctmwindow.component.ts
+++ src/app/proposal/ctm-library/ctmwindow/ctmwindow.component.ts
@@ -7,9 +7,16 @@ import { ProposalService } from "../../../proposal.service";
   styleUrls: ["./ctmwindow.component.css"]
 })
 export class CtmwindowComponent implements OnInit {
+  public description: any;
+  public selectedCateg: any = [];
+  public Others: any;
+  public SelectedCategories: any = [];
+  public showRevenueImpact: any;
+  public tempOthersCode: any;
+  public OthersChecked: any;
   public SelectedCategoriesText: any = "Select Footer Category";
   public SelectCategory: any;
-  public selectLanguage:any;
+  public selectLanguage: any;
   public getLanguages: any = [];
   public footerCategory: any = [];
   constructor(public proposalService: ProposalService) {}
@@ -28,5 +35,33 @@ export class CtmwindowComponent implements OnInit {
       this.footerCategory = data;
     });
   }
-  
+  CheckBoxChecked(cat, isChecked) {
+    if (cat.NeedDescription && isChecked) {
+      this.OthersChecked = true;
+      this.tempOthersCode = cat;
+      $('input[type="checkbox"]').prop("disabled", true);
+      if (cat.HasRevenueImpact) {
+        this.showRevenueImpact = true;
+      }
+    } else {
+      this.OthersChecked = this.showRevenueImpact = false;
+      $('input[type="checkbox"]').removeAttr("disabled");
+    }
+    if (cat.parentId == null) {
+      var temp = [];
+      $("input[name=" + cat.id + "]:checked").prop("checked", false);
+      this.SelectedCategories.forEach(function(category, index) {
+        if (category.parentId != cat.id) {
+          temp.push(category);
+        }
+      });
+      this.SelectedCategories = temp;
+    }
+    if (isChecked) {
+      this.SelectedCategories.push(cat);
+    } else {
+      this.SelectedCategories.splice(this.SelectedCategories.indexOf(cat), 1);
+      this.Others.splice(this.Others.indexOf(cat), 1);
+    }
+  }
 }
