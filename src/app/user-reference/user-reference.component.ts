3fde8d5de356450843ac41a16e00351483ec7bbe
suslavkrishna@gmail.com <suslavkrishna@gmail.com>
2021-08-10 21:53:11 +0530

    fixed user references

--- src/app/user-reference/user-reference.component.ts
+++ src/app/user-reference/user-reference.component.ts
@@ -7,8 +7,8 @@ import { ProposalService } from "../proposal.service";
   styleUrls: ["./user-reference.component.css"]
 })
 export class UserReferenceComponent implements OnInit {
-  public opCenterVales:any = [];
-  public userProgram:any =[];
+  public opCenterVales: any = [];
+  public userProgram: any = [];
   public getLanguages: any = [];
   public user: any = {};
   public config: any = [];
@@ -16,25 +16,26 @@ export class UserReferenceComponent implements OnInit {
 
   ngOnInit(): void {
     this.user.Language = null;
-    this.user.pricingcountry = null;
+    this.user.PricingCountry = null;
+    this.user.OpCenter = null;
+    this.user.Program = null;
     this.domainLoad();
   }
   domainLoad() {
-    this.proposalService.domainConetent().subscribe(data=>{
-      if(Object.keys(data).length >0){
+    this.proposalService.domainConetent().subscribe(data => {
+      if (Object.keys(data).length > 0) {
         this.getLanguages = data.languages;
         this.config = data.countries;
-        this.userProgram = data['domainData'].programs;
-        this.opCenterVales =data['domainData'].operationsCenters;
+        this.userProgram = data["domainData"].programs;
+        this.opCenterVales = data["domainData"].operationsCenters;
       }
-    })
+    });
   }
-  
-  onSubmit() {    
-    this.user['UserName'] = "v-sallampati";
+
+  onSubmit() {
+    this.user["UserName"] = "v-sallampati";
     this.proposalService.updatedomainConetent(this.user).subscribe(data => {
       console.log(data);
-    })
-    
+    });
   }
 }
