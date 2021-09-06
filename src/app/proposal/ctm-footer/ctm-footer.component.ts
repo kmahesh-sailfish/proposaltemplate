3e2c52a6574996479ee15891b05f994c6f4da562
Mahesh <kagithamahesh3@gmail.com>
2021-09-06 20:47:54 +0530

    fixed issues

--- src/app/proposal/ctm-footer/ctm-footer.component.ts
+++ src/app/proposal/ctm-footer/ctm-footer.component.ts
@@ -7,6 +7,11 @@ import { ProposalService } from "../../proposal.service";
   styleUrls: ["./ctm-footer.component.css"]
 })
 export class CtmFooterComponent implements OnInit {
+  public SelectedCategorieIds: any = [];
+  public fileName: any;
+  public reasoning: any;
+  public amtcode: any;
+  public ctmCode: any;
   public footerCategory = [];
   public SelectedCategoriesText: any = "Select Footer Category";
   public SelectCategory: any;
@@ -35,8 +40,10 @@ export class CtmFooterComponent implements OnInit {
       this.footerCategory = data;
     });
   }
+  clearData() { }
+  saveCTMLib(){}
   CheckBoxChecked(cat, isChecked) {
-    if (cat.NeedDescription && isChecked) {
+    if (cat.needDescription && isChecked) {
       this.OthersChecked = true;
       this.tempOthersCode = cat;
       $('input[type="checkbox"]').prop("disabled", true);
@@ -63,5 +70,28 @@ export class CtmFooterComponent implements OnInit {
       this.SelectedCategories.splice(this.SelectedCategories.indexOf(cat), 1);
       this.Others.splice(this.Others.indexOf(cat), 1);
     }
+  }
+  clearCategory() {}
+  SelectCategoryVal() {
+    this.GenerateCTMCodes();
+  }
+  GenerateCTMCodes() {
+    var text = "CTM";
+    var categoryText = null;
+    this.SelectedCategories.forEach((category, index) => {
+      if (categoryText) {
+        categoryText = categoryText + "-" + category.name;
+      } else {
+        categoryText = category.name;
+      }
+      this.SelectedCategorieIds.push(category.id);
+    });
+    text = categoryText ? text + "-" + categoryText : text + "-";
+    // this.CTM.CTMCode = text;
+    // this.CTMLibFile.CTMCode = text;
+    this.SelectCategory = false;
+    this.SelectedCategoriesText = categoryText
+      ? categoryText
+      : "Select Footer Category";
   }
 }
